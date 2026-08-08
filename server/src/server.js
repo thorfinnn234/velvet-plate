a01a9e87bab4842b24715521d5a104b625a638bb
thorfinnn234 <habeeboreoluwa12@gmail.com>
2026-08-08 10:24:09 +0100

    Fixed mongodb issues

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,9 @@
+import dns from "dns";
 import "dotenv/config";
+// ...
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 requireEnv("MONGO_URI");
 requireEnv("JWT_SECRET");
 requireEnv("CLIENT_URL");
@@ -17,6 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/payments", payments);
 app.get("/health", (_, res) => res.json({ ok: true }));
+
 
 app.use("/api/auth", authRoutes);
 
